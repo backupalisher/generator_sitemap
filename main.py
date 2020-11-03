93b4df2b740a097e2e37d57dde0c3f26d2254a3c
Ali <backup.alisher@gmail.com>
2020-11-03 20:44:07 +0300

    fix errors

--- main.py
+++ main.py
@@ -4,7 +4,9 @@ import db_utils
 import file_utils
 import network_utils
 
-url = 'https://part4.info/static/sitemap/'
+url_model = 'https://part4.info/model/'
+url_detail = 'https://part4.info/detail/'
+url_supplies = 'https://part4.info/cartridge/'
 
 
 def gen_model():
@@ -12,16 +14,16 @@ def gen_model():
     list_of_urls = []
     for i in models_id:
         if db_utils.check_options(i[0]):
-            list_of_urls.append(f'{url + i[0]}?tab=options')
+            list_of_urls.append(f'{url_model}{i[0]}?tab=options')
             print(f'\r', i[0], 'options', end='')
         if db_utils.check_parts(i[0]):
-            list_of_urls.append(f'{url + i[0]}?tab=parts')
+            list_of_urls.append(f'{url_model}{i[0]}?tab=parts')
             print(f'\r', i[0], 'parts', end='')
         if db_utils.check_errors(i[0]):
-            list_of_urls.append(f'{url + i[0]}?tab=errors')
+            list_of_urls.append(f'{url_model}{i[0]}?tab=errors')
             print(f'\r', i[0], 'errors', end='')
         if db_utils.check_supplies(i[0]):
-            list_of_urls.append(f'{url + i[0]}?tab=supplies')
+            list_of_urls.append(f'{url_model}{i[0]}?tab=supplies')
             print(f'\r', i[0], 'supplies', end='')
 
     file_utils.save_sitemap('models', list_of_urls)
@@ -32,7 +34,7 @@ def gen_details():
     results = list(set(db_utils.get_id('details')) - set(db_utils.get_model_id_from_details()))
     for res in results:
         print(f'\r', res[0], end='')
-        list_of_urls.append(f'{url + res[0]}')
+        list_of_urls.append(f'{url_detail}{res[0]}')
     file_utils.save_sitemap('details', list_of_urls)
 
 
@@ -41,8 +43,8 @@ def gen_supplies():
     list_of_urls = []
     for i in supplies_id:
         print(f'\r', i[0], end='')
-        if network_utils.check_url(f'{url + i[0]}'):
-            list_of_urls.append(f'{url + i[0]}')
+        if network_utils.check_url(f'{url_supplies}{i[0]}'):
+            list_of_urls.append(f'{url_supplies}{i[0]}')
     file_utils.save_sitemap('supplies', list_of_urls)
 
 
